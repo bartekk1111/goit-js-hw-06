1b7b42eedefe8aa560ee23125c12c9856f83e652
bartekk1111 <bartosz_keskrawiec@wp.pl>
2024-06-16 10:06:04 +0200

    homework completed

--- task-2.js
+++ task-2.js
@@ -1,3 +1,23 @@
+class Storage {
+    #items;
+
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
